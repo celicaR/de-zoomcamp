72ccc2c993c11cb483e88f10e8a87064c1068f4b
celica.rowley@gmail.com <celica.rowley@gmail.com>
2024-02-24 16:23:17 +0000

    fhv models

---
+++ WK4/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging','fhv_tripdata') }}
+  where dispatching_base_num is not null 
+)
+select
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("integer")) }} as dispatching_base_num,
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    
+    -- timestamps
+    IFNULL(SAFE.TIMESTAMP_MILLIS(pickup_datetime),TIMESTAMP_SECONDS(CAST(CAST(pickup_datetime as INT64)/1000000 AS INT64))) as pickup_datetime,
+    IFNULL(SAFE.TIMESTAMP_MILLIS(dropOff_datetime),TIMESTAMP_SECONDS(CAST(CAST(dropOff_datetime as INT64)/1000000 AS INT64))) as dropOff_datetime,
+
+    -- Other trip info
+    {{ dbt.safe_cast("SR_Flag", api.Column.translate_type("integer")) }} as sr_flag,
+    Affiliated_base_number as affiliated_base_number
+from tripdata
+where rn = 1
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
